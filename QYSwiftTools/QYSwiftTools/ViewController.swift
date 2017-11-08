fc5d1abe98090cb132b3299c557f23f82df5a1c6
zqy <zhangqingyu12@126.com>
2017-11-08 17:49:45 +0800

    swiftToolsBegin

---
+++ QYSwiftTools/QYSwiftTools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QYSwiftTools
+//
+//  Created by 张庆玉 on 2017/11/8.
+//  Copyright © 2017年 EricZhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
